b3b136fba83162180826145484ca1e755db32663
jkaddu <jkaddu@yahoo.com>
2019-04-22 13:36:00 -0700

    initial

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // implement your API here
-const express = reqiure('express');
+const express = require('express');
 const db = require('./data/db.js');
 
 const server = express();
@@ -10,7 +10,19 @@ server.get('/', (req, res) => {
     res.send("It's working!")
 });
 
+server.post('/hubs', (req, res) => {
+    const hubInfo = req.body;
+    console.log('request body: ', hubInfo)
+    db.hubs
+    .add(hubInfo)
+    .then(hub => {
+        res.status(201).res.json(hub)
+    })
+    .catch(err => {
+        res.status(500).res.json({ error: error, message: 'Something broke' })
+    })
 
+})
 
 
 server.listen(5000, () => {
