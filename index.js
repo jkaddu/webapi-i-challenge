c161a6880ced85a3a00edefc5e171fc838fd789a
jkaddu <jkaddu@yahoo.com>
2019-04-22 12:50:13 -0700

    initial

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // implement your API here
+const express = reqiure('express');
+const db = require('./data/db.js');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send("It's working!")
+});
+
+
+
+
+server.listen(5000, () => {
+    console.log('\n*** API running on port 5k ***\n')
+});
